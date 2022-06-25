27765a974fe7c4a8cc5457003ad323f5cd12402e
unknown <ka.mierzwinska@gmail.com>
2022-06-25 10:04:03 +0200

    first commit

---
+++ ALXCSharp/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace ALXCSharp
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
